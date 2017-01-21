99f9b85c2a7dc17d79f4d0de31ca425e4bf4a15e
CWilliamsonJr <nucklesix@gmail.com>
2017-01-21 04:06:45 -0500

    add hello2 and hello errors

---
+++ CWilliamsonAssign1/Hello/src/hello/Hello.java
@@ -0,0 +1,24 @@
+/*
+ * Name: Clarence Williamson
+ * Date: 1/21/17
+ * Course: IT: 2650
+ * Assignment #: 1
+ * Prints Hello World
+ */
+package hello;
+
+//Filename Hello2.java
+//Written By: Clarence Williamson
+//Written On: 1/21/17
+
+// Main class for the programs
+public class Hello {
+   /* This class demonstrates the use of the println() method 
+    to print the message Hello, world! 
+    */
+    //Prints Hello World to the screen.
+    public static void main(String[] args) {
+        System.out.println("Hello World, how are you");
+    }
+    
+}
